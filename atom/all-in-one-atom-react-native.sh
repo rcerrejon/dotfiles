8c64fa6667305a9c292ae82fe8f5179f54e61ddf
Rafael Cerrejon <neoline@gmail.com>
2017-10-18 12:58:08 +0200

    Update all-in-one-atom-react-native.sh
    Added some new packages:
    git-blame
    split-diff
    etc..

--- atom/all-in-one-atom-react-native.sh
+++ atom/all-in-one-atom-react-native.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # All-in-One Atom Package for React Native developing
-apm install autoclose-html highlight-selected language-babel linter linter-eslint minimap minimap-highlight-selected pigments seti-ui monokai-seti
+apm install autoclose-html highlight-selected language-babel linter linter-eslint pigments seti-ui monokai-seti git-blame custom-title local-history split-diff
 
-# Atom-beautify: Ctrl+Alt+B
-# Minimap: Absolute Mode: True
 # Seti UI: Compact Mode: True
+# Custom Title Settings > Template: <% if (fileInProject) { %><%= relativeFilePath %> | <%= projectName %><% } else { %><%= filePath %><% } %>
