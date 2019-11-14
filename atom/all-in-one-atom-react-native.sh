ae871cac07d755dcb47f61239c8e3e9ea81aee88
Rafael Cerrejon <neoline@gmail.com>
2019-11-14 14:52:40 +0100

    Delete all-in-one-atom-react-native.sh

--- atom/all-in-one-atom-react-native.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# All-in-One Atom Package for React Native developing
-apm install autoclose-html highlight-selected language-babel linter linter-eslint pigments seti-ui monokai-seti git-blame custom-title local-history split-diff
-
-# Seti UI: Compact Mode: True
-# Custom Title Settings > Template: <% if (fileInProject) { %><%= relativeFilePath %> | <%= projectName %><% } else { %><%= filePath %><% } %>
